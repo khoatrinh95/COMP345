b384676b74e3a511b25d4822eae298e0d8057e1e
Thong Tran <tminhthong1999@gmail.com>
2021-09-25 19:05:46 -0500

    Add OrderList Class

---
+++ OrderList/OrderList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Thong Tran on 2021-09-25.
+//
+
+#include "OrderList.h"
---
+++ OrderList/OrderList.h
@@ -0,0 +1,14 @@
+//
+// Created by Thong Tran on 2021-09-25.
+//
+
+#ifndef COMP345_N11_ORDERLIST_H
+#define COMP345_N11_ORDERLIST_H
+
+
+class OrderList {
+
+};
+
+
+#endif //COMP345_N11_ORDERLIST_H
