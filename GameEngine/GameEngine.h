fb8e412bcd2fabf6585355f5d29753238d8f716f
“Khoa <khoatrinh2108@gmail.com>
2021-10-07 15:52:45 -0400

    #5 Adding copy constructor, assignment, stream insertion to GameEngine

--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -16,46 +16,56 @@ using namespace std;
 class Player;
 enum class Phases{START, MAPLOADED, MAPVALIDATED, PLAYERSADDED, ASSIGNREINFORCEMENT, ISSUEORDERS, EXECUTEORDERS, WIN};
 
-extern Phases * phase;
 
 class StartUp {
 private:
     static void printTitle();
     static void addPlayers();
 public:
-    static void loadMap();
-    static void validateMap();
-    static void createPlayers();
-    static void assignTerritories();
-    static void startUp();
+    // Did not implement copy constructor, assignment overload and ostream insertion as class does not have any member
+    // It does not make sense to have those methods
+    static void loadMap(Phases *phase);
+    static void validateMap(Phases *phase);
+    static void createPlayers(Phases *phase);
+    static void assignTerritories(Phases *phase);
+    static void startUp(Phases *phase);
 };
 
 class Play {
 private:
+
     static void printPlayPhaseGreeting();
 public:
-    static void assignArmies();
-    static void issueOrders();
-    static void executeOrders();
-    static void cleanUp();
-    static void playGame();
+    // Did not implement copy constructor, assignment overload and ostream insertion as class does not have any member
+    // It does not make sense to have those methods
+    static void assignArmies(Phases *phase);
+    static void issueOrders(Phases *phase);
+    static void executeOrders(Phases *phase);
+    static void playGame(Phases *phase);
 };
 
 namespace HelperFunctions {
     void printOptions(vector<string>);
     bool isNumber(const std::string&);
-    void takeInput(Phases);
+    void takeInput(Phases *phase, Phases);
 }
 
 class GameEngine
 {
 public:
-
+    GameEngine();
+    GameEngine(const GameEngine &anotherGameEngine);
+    virtual ~GameEngine();
+    GameEngine& operator = (GameEngine const &anotherGameEngine);
+    friend ostream& operator << (ostream &stream, const GameEngine &gameEngine);
     static Player* getOwnerOf(Territory* territory);
     static void assignToNeutralPlayer(Territory* territory);
-
+    void playGame();
+    static const vector<Player *> &getPlayers();
+    static void addPlayersToList(Player* player);
+    static void clearPlayerList();
 private:
-
+    Phases *phase;
     static std::vector<Player*> players_;
 
 };
