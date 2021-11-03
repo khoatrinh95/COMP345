b4a9e89ba6248f9faca806fcdc425d0e070bdb05
Ricky Lam <50389025+lamricky98@users.noreply.github.com>
2021-11-03 17:48:45 -0400

    assignment 2 part 1 work mostly done? to be tested

---
+++ CommandProcessing/CommandProcessingDriver.h
@@ -0,0 +1,13 @@
+//
+// Created by Ricky Lam on 2021-11-03.
+//
+
+#ifndef COMP345_N11_COMMANDPROCESSINGDRIVER_H
+#define COMP345_N11_COMMANDPROCESSINGDRIVER_H
+
+#include "CommandProcessing.h"
+
+void CommandProcessingDriver(string basicString);
+
+
+#endif //COMP345_N11_COMMANDPROCESSINGDRIVER_H
