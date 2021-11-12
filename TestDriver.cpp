e4523055c3ff2355b32bbc4cb168b64ad641ce12
“Khoa <khoatrinh2108@gmail.com>
2021-11-12 11:44:09 -0500

    final version

--- TestDriver.cpp
+++ TestDriver.cpp
@@ -6,18 +6,15 @@
 
 int main(int argc, char* argv[]){
     cout << "Hello COMP345" << endl;
-//    orderDriver();
-//    PlayerDriver();
-//    CardDriver();
-//    mapDriver();
+    orderDriver();
     GameEngineDriver();
-//    LoggingDriver();
+    LoggingDriver();
 
-    /* uncomment the block below for driver tests for CommandProcessing (part 1)
-     * To run, go to the top bar menu in CLion and click on Run, then go to Edit Configurations,
-     * then in program arguments either type "-console" or "-file ../CommandProcessing/commands.txt"
-     * without quotation marks.
-     *
+//    /* uncomment the block below for driver tests for CommandProcessing (part 1)
+//     * To run, go to the top bar menu in CLion and click on Run, then go to Edit Configurations,
+//     * then in program arguments either type "-console" or "-file ../CommandProcessing/commands.txt"
+//     * without quotation marks.
+//     */
     if (strcmp(argv[1], "-file") == 0) {
         if (argv[2] != NULL) {
             string filename = argv[2];
@@ -30,5 +27,5 @@ int main(int argc, char* argv[]){
     } else {
         CommandProcessingDriver("");
     }
-    */
+
 }
