a9ac047f2a4219b9edb480b4ea1b7daf33668533
Fadi Albasha <fadi.albasha@gmail.com>
2021-11-12 22:40:37 -0500

    Final Final Final version

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -704,24 +704,13 @@ string OrdersList::stringToLog(){
 }
 
 void OrdersList::removeOrder(Order *An_order) {
-//    cout<<"size"<<orders_.size()<<endl;
     for (int i = 0 ; i<orders_.size();i++){
         if (this->orders_.at(i)== An_order){
             orders_.erase(next(begin(orders_), + i));
             delete An_order;
             break;
         }
-
-
-
-//        if (orders_.at(i) == An_order){
-//            delete An_order;
-//            orders_.at(i);
-//            orders_.erase(orders_.begin() + i);
-//            break;
-//        }
     }
-//    cout << "after"<<orders_.size()<<endl;
 }
 
 
