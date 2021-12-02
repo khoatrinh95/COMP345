8db8da7bc96bc342aa95bbda95a72b1283d4beae
Thong Tran <tminhthong1999@gmail.com>
2021-12-02 17:13:28 -0500

    commit to checkout

---
+++ PlayerStrategy/PlayerStrategiesDriver.h
@@ -0,0 +1,13 @@
+//
+// Created by Thong Tran on 2021-12-02.
+//
+
+#ifndef COMP345_N11_PLAYERSTRATEGIESDRIVER_H
+#define COMP345_N11_PLAYERSTRATEGIESDRIVER_H
+
+#include "PlayerStrategy.h"
+#include "../GameEngine/GameEngine.h"
+#include "../Map/Map.h"
+int PlayerStrategiesDriver();
+
+#endif //COMP345_N11_PLAYERSTRATEGIESDRIVER_H
