818de104df91e2a2cd7d557fc3463e429ac31d89
Khoa Trinh <khoatrinh2108@gmail.com>
2021-12-02 13:55:49 -0500

    Revert "Merge branch 'Assignment3' into A3-ThongTran-Part1"
    This reverts commit 5965bd7c649536a1cd98890d764f4fb7a9343a3e, reversing
    changes made to 313e2a69c00254f3c0ec4acb13a3e95792bcf7c9.

--- PlayerStrategy/PlayerStrategy.cpp
+++ PlayerStrategy/PlayerStrategy.cpp
@@ -12,82 +12,72 @@ using namespace std;
 
 
 
+
 // SARAH
-/**
- * it orders the player list of territories according to number of armies
- * @param player
- * @return all territories belong to a player in ascending order according to the number of player
- */
+
+
+
 
 vector<Territory*>  BenevolentPlayerStrategy::toDefend(Player *player) {
-    vector<Territory*>toDefend_Territories;
-    vector<Territory* > orderTerritories = player->getTerritories();
-    sort(orderTerritories.begin(), orderTerritories.end(), [](const Territory* lhs, const Territory* rhs) {
-        return lhs->getNumberOfArmies() < rhs->getNumberOfArmies();
-    });
-    for(auto &territory : orderTerritories){
-        toDefend_Territories.push_back(territory);
-    }
-    return toDefend_Territories;
+    return {};
+//    vector<Territory*>toDefend_Territories;
+//    vector<Territory* > orderTerritories = getPlayer()->getTerritories();
+//    sort(orderTerritories.begin(), orderTerritories.end(), [](const Territory& lhs, const Territory& rhs) {
+//        return lhs.getNumberOfArmies() < rhs.getNumberOfArmies();
+//    });
+//    for(auto &territory : orderTerritories){
+//        toDefend_Territories.push_back(territory);
+//    }
+//    return toDefend_Territories;
 }
-/**
- * the benevolent player does not attack any territories
- * @param player
- * @return empty vector of territories
- */
+
 vector<Territory*>  BenevolentPlayerStrategy::toAttack(Player *player) {
-    vector<Territory*>toAttack_Territories;
-    return toAttack_Territories;
+    return {};
+//    vector<Territory*>toAttack_Territories;
+//    return toAttack_Territories;
 }
 
-/**
- * issue order will go through all benevolent player territories to add armies to the the ones with least number of armies
- * @param player
- */
 void BenevolentPlayerStrategy::issueOrder(Player *player)  {
-    vector<Territory*>toDefend_Territories = toDefend(player);
-    for (auto &ter: toDefend_Territories){
-        cout<<*ter<<endl;
-    }
-    // using all the reinforcement armies for territories that do not have enough armies to protect itself
-    while (player->getReinforcementPool()>0){
-        for (auto &territory : toDefend_Territories){
-            int playerArmies = player->getReinforcementPool();
-            if (playerArmies > 0 ) {
-                int armies = ceil(double(playerArmies) / 3);
-                DeployOrder *deployOrder = new DeployOrder(player, armies, territory);
-                player->getPlayerOrdersList()->add(deployOrder);
-                player->setReinforcementPool(playerArmies - armies);
-            }
-        }
-    }
-    int  j = toDefend_Territories.size()-1;
-
-    for (auto &territory : toDefend_Territories){
-        int i = 0;
-        //  if there is no enough armies in the reinforcement pool use advance order from adjacent territories
-        // to the territories with the lowest number of armies
-        for(  ; i<territory->getNumAdjTerritories();i++){
-            if (territory->getOwner() == territory->getAdjTerritories()[i]->getOwner() &&
-             territory->getNumberOfArmies()<territory->getAdjTerritories()[i]->getNumberOfArmies()-8){
-                AdvanceOrder *advanceOrder = new AdvanceOrder(player, 4, territory->getAdjTerritories()[i],
-                                                              territory);
-                player->getPlayerOrdersList()->add(advanceOrder);
-                break;
-            }
-        }
-        //  if there is no enough armies in the reinforcement pool use Card to issue order
-        // to protect the territories with the lowest number of armies
-        if (i == territory->getNumAdjTerritories() && territory->getNumberOfArmies()<4){
-            for (auto &card: player->getPlayerCards()->getHand()) {
-                if (card!= nullptr && card->getType() == "Airlift") {
-                    card->useCardtoCreateOrder(player,2, toDefend_Territories.at(j),territory);
-                    player->getPlayerCards()->removeCard(card);
-                    j--;
-                }
-            }
-        }
-    }
+//    vector<Territory*>toDefend_Territories = toDefend();
+//    // using all the reinforcement armies for territories that do not have enough armies to protect itself
+//    while (getPlayer()->getReinforcementPool()>0){
+//        for (auto &territory : toDefend_Territories){
+//            int playerArmies = this->getPlayer()->getReinforcementPool();
+//            if (playerArmies > 0 && territory->getNumberOfArmies()<5) {
+//                int armies = ceil(double(playerArmies) / 3);
+//                DeployOrder *deployOrder = new DeployOrder(getPlayer(), armies, territory);
+//                getPlayer()->getPlayerOrdersList()->add(deployOrder);
+//                getPlayer()->setReinforcementPool(playerArmies - armies);
+//            }
+//        }
+//    }
+//    int  j = toDefend_Territories.size()-1;
+//
+//    for (auto &territory : toDefend_Territories){
+//        int i = 0;
+//        //  if there is no enough armies in the reinforcement pool use advance order from adjacent territories
+//        // to the territories with the lowest number of armies
+//        for(  ; i<territory->getNumAdjTerritories();i++){
+//            if (territory->getOwner() == territory->getAdjTerritories()[i]->getOwner() &&
+//             territory->getNumberOfArmies()<territory->getAdjTerritories()[i]->getNumberOfArmies()-8){
+//                AdvanceOrder *advanceOrder = new AdvanceOrder(getPlayer(), 4, territory->getAdjTerritories()[i],
+//                                                              territory);
+//                getPlayer()->getPlayerOrdersList()->add(advanceOrder);
+//                break;
+//            }
+//        }
+//        //  if there is no enough armies in the reinforcement pool use Card to issue order
+//        // to protect the territories with the lowest number of armies
+//        if (i == territory->getNumAdjTerritories() && territory->getNumberOfArmies()<4){
+//            for (auto &card: getPlayer()->getPlayerCards()->getHand()) {
+//                if (card->getType() == "Airlift") {
+//                    AirliftOrder *airliftOrder = new AirliftOrder(getPlayer(), 2,toDefend_Territories.at(j) ,territory);
+//                    getPlayer()->getPlayerOrdersList()->add(airliftOrder);
+//                    j--;
+//                }
+//            }
+//        }
+//    }
 }
 
 
@@ -157,7 +147,7 @@ vector<Territory *> CheaterPlayerStrategy::toAttack(Player *player) {
 }
 
 /**
- * issue order method will go through every adjacent territory and issue an Advance Order with 2x
+ * issue order method will go through every adjacent territory and issue an Advance Order with 10x
  * the amount of army inside that territory -> guarantee conquer
  * @param player
  */
@@ -166,7 +156,7 @@ void CheaterPlayerStrategy::issueOrder(Player *player) {
     for (auto &territory : territories_of_this_player){
         Territory** adjTerritories = territory->getAdjTerritories();
         for (int i = 0; i<territory->getNumAdjTerritories(); i++){
-            int armyToAttack = (adjTerritories[i]->getNumberOfArmies()==0? 10: adjTerritories[i]->getNumberOfArmies()*2);
+            int armyToAttack = adjTerritories[i]->getNumberOfArmies()*10;
             AdvanceOrder *advanceOrder = new AdvanceOrder(player, armyToAttack, territory, adjTerritories[i]);
             player->getPlayerOrdersList()->add(advanceOrder);
             player->setReinforcementPool(player->getReinforcementPool()+armyToAttack);
